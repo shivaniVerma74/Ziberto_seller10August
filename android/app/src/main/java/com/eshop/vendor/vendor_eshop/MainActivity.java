f0ca5345d69f980104bb3361bc1cd1ef66ee08cb
Shivani Verma <shivanialphawizz@gmail.com>
2023-08-10 18:28:47 +0530

    first commit

---
+++ android/app/src/main/java/com/eshop/vendor/vendor_eshop/MainActivity.java
@@ -0,0 +1,6 @@
+package com.alpha_ecommerce.vendor;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+final String appName = 'Ziberto Seller';
+
+final String packageName = 'com.alpha_ecommerce.vendor';
+
+final String iosPackage = 'com.alpha_ecommerce.vendor';
+
+// final String baseUrl = 'https://vendor.eshopweb.store/seller/app/v1/api/';
+//
+// final String jwtKey = "68f05dec6014f68e760c5c5fa3e31bcf391a2e10";
+
+final String baseUrl = 'https://alphawizztest.tk/Ziberto1/seller/app/v1/api/';
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
+
+//timeout limit
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
